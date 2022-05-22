d8d637bc706a76fc8766154843b552774f7c6227
putrird11 <putrirahmadewi77@gmail.com>
2022-05-22 13:39:59 +0700

    Initial Commit

---
+++ Swimming/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swimming
+//
+//  Created by PUTRI RAHMADEWI on 22/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
